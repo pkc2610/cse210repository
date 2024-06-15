b534d7ae43e0ca1f4c2cee6c17c0b6ded004b24a
pkc2610 <phoebekayc@gmail.com>
2024-06-15 11:45:24 -0700

    moving an assignment from a folder I made to the proper repository!

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,46 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main(string[]args)
+
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Console.WriteLine("What was your grade percentage? ");
+        string percent = Console.ReadLine();
+        int gradePercentage = int.Parse(percent);
+
+        string grade = "";
+
+        if (gradePercentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            grade = "D";
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}");
+        
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
+
