41edc50254ffe2fed0e419898d01fa3bfc26aef8
pkc2610 <phoebekayc@gmail.com>
2024-07-08 19:29:48 -0700

    oh but this is bad code

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,8 +4,8 @@ class JOB
 { 
     public string _Company;
     public string  _JobName;
-    public string _StartYear;
-    public string _EndYear;
+    public int _StartYear;
+    public int _EndYear;
 
     public void Display()
 
@@ -15,3 +15,22 @@ class JOB
 }
 }
 // the world's worst code is the code you don't write. This is gonna be the second worst code. Apologies to any TA that might have to look at this.
+
+
+
+class RESUMEbb
+
+{
+    public string Name;
+    public List<_Jobs>;
+
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"{_Jobs}");
+
+        foreach (Job in _jobs)
+        {
+            job.Display();
+    }
+}
+}
