9926da125536e5f087538512896a66bc819f0f6c
pkc2610 <phoebekayc@gmail.com>
2024-07-08 19:03:31 -0700

    well learning means writing really crappy code

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -2,7 +2,12 @@ using System;
 
 class JOB
 { 
-    public string JobName;
-    public string  
+    public string Company;
+    public string  JobName;
+    public string StartYear;
+    public string EndYear;
+    {
+    ConsoleWriteLine(string "JobName (Company) StartYear-EndYear")
+    }
 }
 // the world's worst code is the code you don't write. This is gonna be the second worst code. Apologies to any TA that might have to look at this.
