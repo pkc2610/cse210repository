85170068bc242c9f4da9ece1f8d276e4ae0bbe17
pkc2610 <phoebekayc@gmail.com>
2024-07-08 19:06:00 -0700

    still bad but I think less bad?

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -6,8 +6,12 @@ class JOB
     public string  JobName;
     public string StartYear;
     public string EndYear;
-    {
-    ConsoleWriteLine(string "JobName (Company) StartYear-EndYear")
-    }
+
+    public void Display()
+
+{
+    Console.WriteLine($"{JobName} ({Company}) {StartYear}-{EndYear}")
+
+}
 }
 // the world's worst code is the code you don't write. This is gonna be the second worst code. Apologies to any TA that might have to look at this.
