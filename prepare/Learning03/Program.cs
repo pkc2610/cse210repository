eea8ebc057862f3d6cb37fef4bf21436a3353066
pkc2610 <phoebekayc@gmail.com>
2024-07-08 18:50:30 -0700

    pray for me

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,8 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning03 World!");
-    }
+class JOB
+{ 
+    public string JobName;
+    public string  
 }
+// the world's worst code is the code you don't write. This is gonna be the second worst code. Apologies to any TA that might have to look at this.
